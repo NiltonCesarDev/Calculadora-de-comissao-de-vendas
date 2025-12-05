b01bb7f832ceca04596c58f8de52b1fa3a811455
NiltonCesarDev <ncbombtab@outlook.com>
2025-12-04 21:39:50 -0300

    Pseudocodigo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        /*
+        * Declaracao das variaveis
+        *   double item1 = 239.99;
+        *   double item2 = 129.75;
+        *   double item3 = 99.95;
+        *   double item4 = 350.89;
+        *   double comissao = 0;
+        *
+        * Criacao do objeto input
+        * exibe uma mensagem solicitando o item vendido
+        * armazena o valor em uma variavel item
+        *
+        * Enquanto item for diferente de -1
+        *   puxa o valor do item, armazena e soma a comissao
+        *   exibe a mesma mensagem
+        *
+        * Calcula a comissao = R$ 200,00 + 9% da variavel
+        * Exibe o valor da comissao
+        * */
     }
 }
